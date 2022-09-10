2571e464f33ff16f64d75ee787773ccf21267b6a
Swapnil639 <swapnilpatil631997@gmail.com>
2022-09-10 19:37:22 +0530

    Added :Check analyze mood

--- src/main/java/com/bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -2,5 +2,10 @@ package com.bridgelabz;
 
 
 public class MoodAnalyzer {
-   
+    public String analyzeMood(String message) {
+        if (message.contains("Sad"))
+            return "SAD";
+        else
+            return "HAPPY";
+    }
 }
--- src/test/java/com/bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -1,5 +1,20 @@
 package com.bridgelabz;
 
-public class MoodAnalyzerTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class MoodAnalyzerTest {
+    static MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+
+    @Test
+    public void givenMessage_whenSad_ShouldReturnSad() {
+        String mood = moodAnalyzer.analyzeMood("Sad");
+        Assert.assertEquals("SAD", mood);
+    }
+
+    @Test
+    public void givenMessage_whenHappy_ShouldReturnHappy() {
+        String mood = moodAnalyzer.analyzeMood("Happy");
+        Assert.assertEquals("HAPPY", mood);
+    }
 }
